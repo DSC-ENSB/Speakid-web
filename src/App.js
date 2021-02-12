6943078c964069f6ba0861a1b5c6c6abb876c58c
Salem-Abderaouf <salamasalem84@gmail.com>
2021-02-12 01:59:22 +0100

    Create and update routers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import './static/App.css';
+import Hero from './screens/Hero'
+import Register from './screens/Sign-up'
+import Login from './screens/Sign-in'
+import Profile from './screens/Profile';
+import db from './config/db'
+import User from './screens/User';
 
-function App() {
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false)
+
+  const listenUser = () => {
+    const usersRef = db.database();
+    db.auth().onAuthStateChanged((user) => {
+      if (user) {
+        usersRef
+          .ref(`u/${user.uid}`)
+          .once('value', () => {
+            setAuthenticated(true)
+          })
+          .catch(() => {
+            setAuthenticated(false)
+          });
+      } else {
+        setAuthenticated(false)
+      }
+    });
+  }
+  useEffect(() => {
+    listenUser()
+  }, [authenticated])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {authenticated ? <Redirect to='/user' /> : <Redirect to='/' />}
+      <Switch>
+        {
+          authenticated ?
+            <>
+              <Route path="/user" component={User} exact></Route>
+              <Route path="/profile" component={Profile} exact></Route>
+            </>
+            :
+            <>
+              <Route path="/" component={Hero} exact></Route>
+              <Route path="/register" component={Register} exact></Route>
+              <Route path="/login" component={Login} exact></Route>
+            </>
+        }
+      </Switch>
+    </Router>
   );
 }
 
