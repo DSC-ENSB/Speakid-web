7f865e85b77e44eac733387ffb8c0bd2637abac5
Salem-Abderaouf <salamasalem84@gmail.com>
2021-03-09 10:47:00 +0100

    Add Sidebar component

--- src/screens/User.js
+++ src/screens/User.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Dashboard from '../components/Dashboard';
 import Navbar from '../components/Navbar';
+import SidePanal from '../components/SidePanal'
 
 const User = () => {
   return (
     <>
       <Navbar />
+      <SidePanal />
       <Dashboard />
     </>)
 }
